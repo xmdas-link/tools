fe9176984a83bc450e2df09628b61ccc5ceef645
zengshuyan <miyula1988@gmail.com>
2019-09-26 08:37:04 +0800

    补充时间类型

--- time_tool/time.go
+++ time_tool/time.go
@@ -9,6 +9,7 @@ const Layout_YYYY_MM_DD = "2006-01-02"
 const Layout_YMDHIS = "20060102150405"
 const Layout_YMD_HIS = "2006-01-02 15:04:05"
 const Layout_YYYY_MM = "2006-01"
+const Layout_YMD = "2006-01-02"
 
 var (
 	location *time.Location
