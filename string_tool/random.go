6a55aa1459636698f6461d3ab92c8b3e9a0c867f
zengshuyan <miyula1988@gmail.com>
2019-12-03 15:10:38 +0800

    增加随机数返回格式

--- string_tool/random.go
+++ string_tool/random.go
@@ -16,3 +16,27 @@ func GetRandomString(length int) string {
 	}
 	return string(result)
 }
+
+func GetRandomNumber(length int) string {
+	str := "0123456789"
+	bytes := []byte(str)
+	result := make([]byte, length)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result[i] = bytes[r.Intn(len(bytes))]
+		//result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func GetRandomChar(length int) string {
+	str := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := make([]byte, length)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result[i] = bytes[r.Intn(len(bytes))]
+		//result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
